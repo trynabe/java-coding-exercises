1f541b1ac7864390d02f1258b3f2206c9357aee2
Trynabe <itstrynabe@email.com>
2026-01-18 20:05:08 +0700

    update lab week 2

--- Week 2 - Fundamental Data Types & Decisions/Lab2Q4.java
+++ Week 2 - Fundamental Data Types & Decisions/Lab2Q4.java
@@ -1,3 +1,13 @@
 public class Lab2Q4 {
-    
+    public static void main(String[] args) {
+
+        String username = "Trynabe";
+        String password = "1234";
+
+        if (username.equals("Trynabe") && password.equals("1234")){
+            System.out.println("You're now logged in.");
+        } else {
+            System.out.println("Sorry, your username or password is incorrect");
+        }
+    }
 }
