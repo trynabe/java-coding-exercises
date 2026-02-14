0764921f6fad2b5a50b016f5c3e60cb43570e409
trynabe <itstrynabe@gmail.com>
2026-02-14 13:24:37 +0700

    updated week 6

---
+++ Week 6 - Designing Classes/DateTester.java
@@ -0,0 +1,48 @@
+public class DateTester{
+	public static String strLeapYear(boolean leapYear){
+		if(leapYear)
+			return "is a leap year";
+		else
+			return "is not a leap year";
+	}
+	
+	public static void main(String[] args){
+		MyDate a = new MyDate();
+		System.out.println("Object Number (a): " + a.getObjectNumber());
+		System.out.println("a's Date: " + a.toString());
+		a.setDate(2000, 4, 13);
+		System.out.println("a's Date: " + a.toString());
+		System.out.println("a's year is " + a.getYear() + ", which " + DateTester.strLeapYear(MyDate.isLeapYear(a.getYear())) + ".\n");
+
+		MyDate b = new MyDate(2020, 2, 28);
+		System.out.println("Object Number (b): " + b.getObjectNumber());
+		System.out.println("b's Date: " + b.toString());
+		System.out.println("b's year is " + b.getYear() + ", which " + DateTester.strLeapYear(MyDate.isLeapYear(b.getYear())) + ".\n");
+
+		MyDate c = new MyDate(2021, 3, 2);
+		System.out.println("Object Number (c): " + c.getObjectNumber());
+		System.out.println("c's Date: " + c.toString());
+		System.out.println("c's year is " + c.getYear() + ", which " + DateTester.strLeapYear(MyDate.isLeapYear(c.getYear())) + ".\n");
+		
+		//Uncomment this for the challenge task
+		
+		System.out.println("Challenge Bonus ::");
+		System.out.println("a's Date: " + a.previousDay().toString());
+		System.out.println("a's Date: " + a.nextDay().toString());
+		System.out.println("a's Date: " + a.previousMonth().toString());
+		System.out.println("a's Date: " + a.nextMonth().toString());
+		
+		System.out.println("b's Date: " + b.nextDay().toString());
+		System.out.println("b's Date: " + b.nextDay().toString());
+		System.out.println("b's Date: " + b.nextYear().toString());
+		System.out.println("b's Date: " + b.nextMonth().toString());
+		System.out.println("b's Date: " + b.previousYear().toString());
+		
+		System.out.println("c's Date: " + c.previousDay().toString());
+		System.out.println("c's Date: " + c.previousDay().toString());
+		System.out.println("c's Date: " + c.previousYear().toString());
+		System.out.println("c's Date: " + c.nextDay().toString());
+		System.out.println("c's Date: " + c.previousYear().toString());
+		
+	}
+}
---
+++ Week 6 - Designing Classes/MyDate.java
@@ -0,0 +1,182 @@
+public class MyDate {
+    // Instance Variable
+    int year;
+    int month;
+    int day;
+    int objectNumber;
+
+    // Static Class Variable
+    public static int objectCounter;
+    public static String[] strMonths = {"January","February","March","April","May","June","July","August","September","October","November","December"};
+
+    // Constructors
+    public MyDate(int aYear, int aMonth, int aDay){
+        year = aYear;
+        month = aMonth;
+        day = aDay;
+        objectCounter ++;
+        objectNumber = objectCounter;
+    }
+
+    // Default Constructor
+    public MyDate(){
+        year = 1900;
+        month = 1;
+        day = 1;
+        objectCounter ++;
+        objectNumber = objectCounter;
+    }
+
+    // Static Method
+    public static boolean isLeapYear(int year){
+        if (year % 4 != 0){
+            return false;
+        } else if (year % 100 == 0 && year % 400 != 0){
+            return true;
+        } else if (year % 400 == 0 && year % 100 != 0){
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    // Instance Methods
+    public int getObjectNumber(){
+        return objectNumber;
+    }
+
+    public void setDate(int aYear, int aMonth, int aDay){
+        year = aYear;
+        month = aMonth;
+        day = aDay;
+    }
+
+    public void setYear(int aYear){
+        year = aYear;
+    }
+
+    public void setMonth(int aMonth){
+        month = aMonth;
+    }
+
+    public void setDay(int aDay){
+        day = aDay;
+    }
+
+    public int getYear(){
+        return year;
+    }
+
+    public int getMonth(){
+        return month;
+    }
+
+    public int getDay(){
+        return day;
+    }
+
+    public String toString(){
+        return (day+" "+strMonths[month-1]+" "+year);
+    }
+
+    // Challenge Bonus (Optional)
+    public MyDate nextDay() {
+        if (month == 12 && day == 31) {
+            year = year + 1;
+            month = 1;
+            day = 1;
+        } else {
+            if (month == 4 || month == 6 || month == 9 || month == 11) {
+                if (day == 30) {
+                    month = month + 1;
+                    day = 1;
+                } else {
+                    day = day + 1;
+                }
+            } else if (month != 2) {
+                if (day == 31) {
+                    month = month + 1;
+                    day = 1;
+                } else {
+                    day = day + 1;
+                }
+            } else {
+                if (isLeapYear(year) && day == 29) {
+                    month = month + 1;
+                    day = 1;
+                } else if (!isLeapYear(year) && day == 28) {
+                    month = month + 1;
+                    day = 1;
+                } else {
+                    day = day + 1;
+                }
+            }
+        }
+        return this;
+    }
+
+    public MyDate previousDay() {
+        if (month == 1 && day == 1) {
+            year = year - 1;
+            month = 12;
+            day = 31;
+        } else {
+            if (month == 5 || month == 7 || month == 10 || month == 12) {
+                if (day == 1) {
+                    month = month - 1;
+                    day = 30;
+                } else {
+                    day = day - 1;
+                }
+            } else if (month != 3) {
+                if (day == 1) {
+                    month = month - 1;
+                    day = 31;
+                } else {
+                    day = day - 1;
+                }
+            } else {
+                if (isLeapYear(year) && day == 1) {
+                    month = month - 1;
+                    day = 29;
+                } else if (day == 1) {
+                    month = month - 1;
+                    day = 28;
+                } else {
+                    day = day - 1;
+                }
+            }
+        }
+        return this;
+    }
+
+    public MyDate nextMonth() {
+        if (month == 12) {
+            year++;
+            month = 1;
+        } else {
+            month++;
+        }
+        return this;
+    }
+
+    public MyDate previousMonth() {
+        if (month == 1) {
+            year--;
+            month = 12;
+        } else {
+            month--;
+        }
+        return this;
+    }
+
+    public MyDate nextYear() {
+        year++;
+        return this;
+    }
+
+    public MyDate previousYear() {
+        year--;
+        return this;
+    }
+}
